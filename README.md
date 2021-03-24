4ab98b6b8fe6afbed5c9a5640612f0f7f079d813
David Wengier <git@wengier.com>
2021-03-24 22:08:11 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Linkable Diff
+
+[![License: MIT](https://img.shields.io/github/license/davidwengier/LinkableDiff?color=blue "License: MIT")](https://choosealicense.com/licenses/mit/)
+[![Main build status](https://github.com/davidwengier/LinkableDiff/workflows/DeployToGitHubPages/badge.svg "Build status")](https://github.com/davidwengier/LinkableDiff/actions?query=workflow%3ADeployToGitHubPages)
+[![Join the Discord](https://img.shields.io/discord/709643112636612658?label=Discord "Join the Discord")](https://discord.gg/Yt5B58b)
+
+## An editable diff with a permalink
+
+Just a diff editor that gives you a perma-link for your diff.
+
+# Try it live: https://wengier.com/LinkableDiff
---
+++ Runner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+#nullable enable
+
+namespace LinkableDiff
+{
+    internal static class Runner
+    {
+        public static string Compress(string version1, string version2)
+        {
+            var separator = (char)7;
+            return Compress(version1 + separator + version2);
+
+            static string Compress(string input)
+            {
+                using var ms = new MemoryStream();
+                using (var compressor = new DeflateStream(ms, CompressionLevel.Optimal))
+                {
+                    var inputBytes = Encoding.Unicode.GetBytes(input);
+                    compressor.Write(inputBytes);
+                }
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        public static string Uncompress(string slug)
+        {
+            try
+            {
+                var bytes = Convert.FromBase64String(slug);
+
+                using var ms = new MemoryStream(bytes);
+                using (var compressor = new DeflateStream(ms, CompressionMode.Decompress))
+                using (var sr = new StreamReader(compressor, Encoding.Unicode))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
